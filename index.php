b5b53f58954b0f231f3fbe478fd2726234c4a000
Edu Ruiz <contato@eduruiz.com.br>
2014-05-31 23:06:33 -0300

    first commit
    working in progress… =)

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Eduruiz</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="assets/css/main.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <h1 class="logo ir">
+            Edu Ruiz Portfólio
+        </h1>
+        <span class="tricky"></span>
+
+        <section class="portfolio">
+            <div class="container">
+                <a href="http://www.indiosnativosvalvulados.com.br" class="job">
+                  <figure><img src="assets/images/jobs/indios.jpg" alt="índios nativos valvulados"></figure>  
+                  <figcaption>índios nativos valvulados</figcaption>
+                </a>
+                <a href="http://www.pepperbox.com.br" class="job">
+                  <figure><img src="assets/images/jobs/pepper.jpg" alt="pepper box"></figure>  
+                  <figcaption>pepper box</figcaption>
+                </a>
+                <a href="http://www.pinusbr.com.br" class="job">
+                  <figure><img src="assets/images/jobs/pinusbr.jpg" alt="pepper box"></figure>  
+                  <figcaption>pepper box</figcaption>
+                </a>
+            </div>
+        </section>
+        
+        <div class="white">
+            <div class="container">
+                <section class="oquesei">
+                        <h1>o que sei:</h1>
+                        <div>
+                            <img src="assets/images/html5.png" alt="html5">
+                            <img src="assets/images/js.png" alt="js">
+                            <img src="assets/images/css3.png" alt="css3">
+                            <img src="assets/images/sass.png" alt="sass">
+                            <img src="assets/images/compass.png" alt="compass">
+                            <img src="assets/images/php.png" alt="php">
+                        </div>
+                </section>
+                
+                <div class="clearfix"></div>
+
+                <section class="oqueuso">
+                        <h1>o que uso:</h1>
+                        <div>
+                            <img src="assets/images/sublime.png" alt="sublime text">
+                            <img src="assets/images/codekit.png" alt="codekit">
+                            <img src="assets/images/grunt.png" alt="grunt js">
+                            <img src="assets/images/codeigniter.png" alt="codeigniter">
+                            <img src="assets/images/sequelpro.png" alt="sequel pro">
+                            <img src="assets/images/transmit.png" alt="transmit app">
+                        </div>
+                </section>
+            </div>
+        </div>
+
+        <section class="ondeestou">
+            <div class="container">
+                <h1>onde estou:</h1>
+                <div>
+                    <a href=""><?php include 'assets/images/github.svg'; ?></a>
+                    <!--<a href=""> <img src="assets/images/github.png" alt="github"></a> -->
+                    <a href=""><?php include 'assets/images/bitbucket.svg'; ?></a>
+                    <!--<a href=""> <img src="assets/images/bitbucket" alt="bitbucket"></a> -->
+                    <a href=""><?php include 'assets/images/stackoverflow.svg'; ?></a>
+                    <!--<a href=""> <img src="assets/images/stackoverflow" alt="stackoverflow"></a> -->
+                    <a href=""><?php include 'assets/images/skype.svg'; ?></a>
+                    <!--<a href=""> <img src="assets/images/skype" alt="skype"></a> -->
+                    <a href=""><?php include 'assets/images/email.svg'; ?></a>
+                    <!--<a href=""> <img src="assets/images/email" alt="email"></a> -->
+                    <a href=""><?php include 'assets/images/tel.svg'; ?></a>
+                    <!--<a href=""> <img src="assets/images/tel" alt="telefone"></a> -->
+                </div>
+            </div>
+        </section>
+        <footer></footer>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="assets/js/plugins.js"></script>
+        <script src="assets/js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
